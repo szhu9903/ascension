49b0845e1cdc1670d0b2dfc94e9c5b73620760ac
15994092160 <1781082307@qq.com>
2019-06-07 07:48:55 +0800

    6.7pythoncookbook

---
+++ cookbook/1.py
@@ -0,0 +1,14 @@
+recode = [
+    ('foo',1,2),
+    ('bar','hello'),
+    ('foo',3,4)
+]
+def do_foo(x,y):
+    print('foo',x,y)
+def do_bar(s):
+    print('bar',s)
+for tag,*args in recode:
+    if tag == 'foo':
+        do_foo(*args)
+    elif tag == 'bar':
+        do_bar(*args)
