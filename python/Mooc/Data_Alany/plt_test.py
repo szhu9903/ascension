5246a818849c9b448f337fbbcfe53feef7b5533b
15994092160 <1781082307@qq.com>
2020-05-23 16:20:39 +0800

    matploylib sin

--- python/Mooc/Data_Alany/plt_test.py
+++ python/Mooc/Data_Alany/plt_test.py
@@ -6,19 +6,43 @@
 # plt.axis([-1,10,0,7])
 # plt.show()
 
-import matplotlib.pyplot  as  plt
-import numpy as np
-from random import choice
+# import matplotlib.pyplot  as  plt
+# import numpy as np
+# from random import choice
+# import matplotlib
+#
+# #全局设置字体
+# # matplotlib.rcParams['font.family'] = 'SimHei'
+# # matplotlib.rcParams['font.size'] = 16
+#
+# num_all = []
+# index = 1.5
+# rgb = ['b--','g-.','r:','c:','m:']
+# for i in range(5):
+#     a = np.arange(10)
+#     plt.plot(a,a*index,choice(rgb))
+#     index += 1
+#
+# #局部设置字体
+# plt.xlabel('X轴',fontproperties='SimHei',fontsize=16)
+# plt.ylabel('Y轴',fontproperties='Kaiti',fontsize=16)
+# plt.show()
 
-num_all = []
-index = 1.5
-rgb = ['b','g','r','c','m']
-for i in range(5):
-    a = np.arange(10)
-    plt.plot(a,a*index,choice(rgb))
-    index += 1
-plt.xlabel('X')
-plt.ylabel('Y')
+
+
+# 正弦波cos
+import matplotlib.pyplot as plt
+import numpy as np
+
+num = np.arange(-2*np.pi,2*np.pi,0.02)
+plt.plot(num,np.sin(num),'r--')
+
+plt.xlabel('横轴：时间',fontproperties='SimHei',fontsize=16)
+plt.ylabel('纵轴：振幅',fontproperties='SimHei',fontsize=16)
+plt.title(r'正弦$y=sin(\pi x)$',fontproperties='SimHei',fontsize=25)
+
+plt.axis([-3*np.pi,3*np.pi,-2,2])
+plt.grid(True)
 plt.show()
 
 
