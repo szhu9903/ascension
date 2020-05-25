f465763aa02329d77b7c52e494efbab310db4de1
15994092160 <1781082307@qq.com>
2020-05-25 21:42:48 +0800

    matploylib sin

--- python/Mooc/Data_Alany/plt_test.py
+++ python/Mooc/Data_Alany/plt_test.py
@@ -31,18 +31,44 @@
 
 
 # 正弦波cos
+# import matplotlib.pyplot as plt
+# import numpy as np
+#
+# num = np.arange(-2*np.pi,2*np.pi,0.02)
+# plt.plot(num,np.sin(num),'r--')
+#
+# plt.xlabel('横轴：时间',fontproperties='SimHei',fontsize=16)
+# plt.ylabel('纵轴：振幅',fontproperties='SimHei',fontsize=16)
+# plt.title(r'正弦$y=sin(\pi x)$',fontproperties='SimHei',fontsize=25)
+#
+# plt.axis([-3*np.pi,3*np.pi,-2,2])
+# plt.grid(True)
+# plt.show()
+
+
+# plt基础函数，绘制饼形图
 import matplotlib.pyplot as plt
-import numpy as np
+import matplotlib
 
-num = np.arange(-2*np.pi,2*np.pi,0.02)
-plt.plot(num,np.sin(num),'r--')
+matplotlib.rcParams['font.family'] = 'SimHei'
+matplotlib.rcParams['font.size'] = 16
 
-plt.xlabel('横轴：时间',fontproperties='SimHei',fontsize=16)
-plt.ylabel('纵轴：振幅',fontproperties='SimHei',fontsize=16)
-plt.title(r'正弦$y=sin(\pi x)$',fontproperties='SimHei',fontsize=25)
+labels = '一月份','二月份','三月份','四月份','五月份','六月份'
+sizes = [10,20,30,5,10,25]
+explode = (0,0,0,0,0.1,0)
 
-plt.axis([-3*np.pi,3*np.pi,-2,2])
-plt.grid(True)
+
+plt.pie(sizes,explode=explode,labels=labels,autopct='%1.1f%%',
+        shadow=False,startangle=90)
+plt.axis('equal')
 plt.show()
+
+
+
+
+
+
+
+
 
 
