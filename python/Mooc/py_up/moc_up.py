92712d7a763562a74cc58b91906517d13e8dadbd
15994092160 <1781082307@qq.com>
2020-07-11 23:20:25 +0800

    高阶函数

--- python/Mooc/py_up/moc_up.py
+++ python/Mooc/py_up/moc_up.py
@@ -1,11 +1,26 @@
+"""
+python 高阶函数 ：接收函数为参数函数 map
+"""
+
+
 
 def format_name(name):
     return name[0].upper() + name[1:].lower()
 
-if __name__ == '__main__':
-    new_name = map(format_name,['admin','LISA','brRt'])
-    print(list(new_name))
+from functools import reduce
+# reduce 接收两个参数，将第一轮结果继续
+def prod(num_a,num_b):
+    return num_a*num_b
 
+
+if __name__ == '__main__':
+
+    # function list
+    # new_name = map(format_name,['admin','LISA','brRt'])
+    # print(list(new_name))
+
+    new_num = reduce(prod,[2,4,5,7,12])
+    print(new_num)
 
 
 
