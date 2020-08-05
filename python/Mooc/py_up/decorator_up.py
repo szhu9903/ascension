335e4eef1c721141e1e0b2f60fdbb4b0eb573e95
15994092160 <1781082307@qq.com>
2020-08-05 20:48:44 +0800

    py_up factorial

--- python/Mooc/py_up/decorator_up.py
+++ python/Mooc/py_up/decorator_up.py
@@ -1,10 +1,20 @@
 import time
+from functools import reduce
+import datetime
 
 def performance(f):
-    ???
+    def fun(*args,**kwargs):
+        date = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        print(date)
+        start_date = time.time()
+        res = f(*args,**kwargs)
+        end_date = time.time()
+        print(end_date-start_date)
+        return res
+    return fun
 
 @performance
 def factorial(n):
     return reduce(lambda x,y: x*y, range(1, n+1))
 
-print factorial(10)
+print(factorial(10))
