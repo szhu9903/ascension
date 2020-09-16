f0c4a68fef1a0a9cb1d2b66165f7b0d1095d0332
15994092160 <1781082307@qq.com>
2020-09-16 21:38:31 +0800

    websocket

--- python/web_socket/client.py
+++ python/web_socket/client.py
@@ -1,14 +1,13 @@
 import asyncio
 import websockets
 
-async def hello():
-    async with websockets.connect('ws://localhost:8765') as websocket:
-        async for message in websocket:
-            print(message)
+async def hello(uri):
+    async with websockets.connect(uri) as websocket:
+        while True:
+            name = input('name:')
+            await websocket.send(name)
+            serve_data = await websocket.recv()
+            print(serve_data)
 
 if __name__ == '__main__':
-    asyncio.get_event_loop().run_until_complete(hello())
-
-
-
-
+    asyncio.get_event_loop().run_until_complete(hello('ws://localhost:8765'))
--- python/web_socket/server.py
+++ python/web_socket/server.py
@@ -1,15 +1,12 @@
 import asyncio
 import websockets
 
-async def server(websocket, path):
-    while True:
-        message = input('name:')
-        await websocket.send(message)
+async def hello(websocket, path):
+    async for message in websocket:
+        greeting = "hello %s"%message
+        await websocket.send(greeting)
 
 
 if __name__ == '__main__':
-    start_server = websockets.serve(server, 'localhost', 8765)
-    asyncio.get_event_loop().run_until_complete(start_server)
+    asyncio.get_event_loop().run_until_complete(websockets.serve(hello,'localhost',8765))
     asyncio.get_event_loop().run_forever()
-
-
