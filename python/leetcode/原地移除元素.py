5c476c0b3b8b814c09ccbc91f700efda6d3f41e0
szhu9903 <szhu9903@gmail.com>
2020-12-25 17:49:54 +0800

    原地删除，不开辟新的内存空间

--- python/leetcode/原地移除元素.py
+++ python/leetcode/原地移除元素.py
@@ -4,9 +4,8 @@
 不要使用额外的数组空间，你必须仅使用 O(1) 额外空间并 原地 修改输入数组。
 
 元素的顺序可以改变。你不需要考虑数组中超出新长度后面的元素。
+
 """
-
-
 def removeElement(nums, val):
     num = 0
     if len(nums)==0:return 0
@@ -15,9 +14,6 @@ def removeElement(nums, val):
             nums[num] = nums[i]
             num +=1
     return num
-
-
-
 
 if __name__ == '__main__':
     res = removeElement([0, 1, 2, 2, 3, 0, 4, 2], 2)
