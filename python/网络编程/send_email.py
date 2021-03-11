041a3b73995daebaecd5623af8220c27ee35f85b
szhu9903 <szhu9903@gmail.com>
2021-03-11 15:22:04 +0800

    SMTP

--- python/网络编程/send_email.py
+++ python/网络编程/send_email.py
@@ -1,19 +1,19 @@
+from python import logger
+
 # SMTP 服务
 import smtplib
-# 邮件头部
-import email.mime.multipart
-# 邮件内容
-import email.mime.text
+from email.mime.text import MIMEText
+from email.header import Header
 
 email_user = '1781082307@qq.com'
 email_recv = 'szhu9903@gmail.com'
-email_password = 'dkflesshnbzhbafc'
+email_code = 'dkflesshnbzhbafc'
 email_server = 'smtp.qq.com'
+email_port = 465
 
-message = email.mime.multipart.MIMEMultipart()
-message['from'] = email_user
-message['to'] = email_recv
-message['subject'] = '测试邮件处理'
+stmp = smtplib.SMTP_SSL(email_server, email_port)
+stmp.login(email_user, email_code)
+logger.info('服务器连接成功')
 
 content = """
 int main()
@@ -23,12 +23,17 @@ int main()
     return 0;
 }
 """
-content_add = email.mime.text.MIMEText(content)
-message.attach(content_add)
+# 拼装数据体
+message = MIMEText(content, 'plain', 'utf-8')
+# 发件人
+message['From'] = Header('Python Smtp Szhu', 'utf-8')
+# 收件人
+message['To'] = Header('Szhu', 'utf-8')
+# 标题
+message['Subject'] = Header('Python Smtp 本地测试', 'utf-8')
 
-smtp = smtplib.SMTP_SSL(email_server, 456)
-smtp.login(email_user, email_password)
-recv_data = smtp.sendmail(email_user, email_recv, message.as_string())
-smtp.close()
-
-
+try:
+    stmp.sendmail(email_user, email_recv, message.as_string())
+except Exception as err:
+    logger.error('失败%s' % err)
+logger.info('发送成功。')
