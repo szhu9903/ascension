6605d795a393a7390c27babf1cb275116117711d
szhu9903 <szhu9903@gmail.com>
2021-03-25 17:26:31 +0800

    unittest规范日常测试单元

---
+++ python_up/dailyTest.py
@@ -0,0 +1,53 @@
+import os
+import struct
+import unittest
+import logging
+import re
+
+# 添加日志设置等级，
+logger = logging.getLogger('logName')
+logger.setLevel(logging.DEBUG)
+# 设置Handle, 在Handle中配置日志输出格式
+loggerHandle = logging.StreamHandler()
+# 注意日志格式设置中时间，是","号隔开的
+loggerFormatter = logging.Formatter("[%(asctime)s] %(levelname)s %(message)s", '%Y-%m-%d %H:%M:%S')
+loggerHandle.setLevel(logging.DEBUG)
+loggerHandle.setFormatter(loggerFormatter)
+# 将设置好的Handle添加进设置的日志中
+logger.addHandler(loggerHandle)
+
+
+class DailyTest(unittest.TestCase):
+    def setUp(self):
+        logger.info('start test...')
+
+    def test_str_split(self):
+        data_str = b'\x00C35\x0011\xc2\xb111\x0011\xc2\xb111\x00\x0011\xc2\xb111\x00\x0011\xc2\xb111\x00'
+        res_list, data_str = self.get_bytes_str(data_str, 5, [])
+        print(res_list, data_str)
+
+    def test_re_C30(self):
+        st = 'kfklfpdsCfsC35555afs'.upper()
+        b = re.search(r'C\d{2}', st)
+        self.assertIsNotNone(b)
+        print(b.group())
+
+
+    def get_bytes_str(self, data_str, split_num, data_list):
+        if split_num <= 0:
+            return data_list,data_str
+        else:
+            res_index = data_str.index(bytes([0]))
+            res = data_str[:res_index].decode('utf-8')
+            data_list.append(res)
+            return self.get_bytes_str(data_str[res_index + 1:], split_num-1, data_list)
+
+
+if __name__ == '__main__':
+    unittest.main()
+
+
+
+
+
+
